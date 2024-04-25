b94f37ded6e4251a02cc4d0fd0aef0c5bba6030f
Shadow <oleksandr.vpro@gmail.com>
2024-04-25 18:51:57 +0300

    Add custom breakpoints

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,7 @@ module.exports = {
   theme: {
     extend: {
       colors: {
+        "bxm-card": "#1F1F1F",
         "bxm-black": "#000000",
         "bxm-gray": "#B7B7B7",
         "bxm-red": "#E20613",
@@ -13,6 +14,15 @@ module.exports = {
       letterSpacing: {
         lighter: "calc(1em / -50)",
         light: "calc(1em / -33)",
+      },
+      screens: {
+        xxs: { min: "360px" },
+        xs: { min: "540px" },
+        s: { min: "768px" },
+        md: { min: "900px" },
+        lg: { min: "1100px" },
+        "has-hover": { raw: "(hover: hover)" },
+        "no-hover": { raw: "(hover: none)" },
       },
     },
   },
