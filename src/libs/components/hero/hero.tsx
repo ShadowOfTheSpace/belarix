07fd49057e65e133bac8333e3705350e48a8fc12
Shadow <oleksandr.vpro@gmail.com>
2024-04-26 00:43:21 +0300

    Fixed build errors

--- src/libs/components/hero/hero.tsx
+++ src/libs/components/hero/hero.tsx
@@ -4,9 +4,9 @@ const Hero: React.FC = () => {
   return (
     <section className="mt-[40px] sm:mt-[25px] md:mt-[19px] rounded-[10px] md:rounded-[20px] overflow-hidden">
       <img
+        alt="hero"
         className="md:w-[1840px] md:aspect-[1840/700] aspect-[340/180] object-cover"
         src={heroImage}
-        alt="hero-image"
       />
     </section>
   );
