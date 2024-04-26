61f018fed5915016ef46a2868a2dc8b88cce9fcf
Shadow <oleksandr.vpro@gmail.com>
2024-04-26 16:59:50 +0300

    Add icons

--- src/libs/components/icon/libs/maps/icon-name-to-svg/icon-name-to-svg.map.ts
+++ src/libs/components/icon/libs/maps/icon-name-to-svg/icon-name-to-svg.map.ts
@@ -1,3 +1,8 @@
+import { ReactComponent as ArchIcon } from "@assets/icons/arch.svg";
+import { ReactComponent as DiamondIcon } from "@assets/icons/diamond.svg";
+import { ReactComponent as FlowerIcon } from "@assets/icons/flower.svg";
+import { ReactComponent as HexagonIcon } from "@assets/icons/hexagon.svg";
+import { ReactComponent as TriangleIcon } from "@assets/icons/triangle.svg";
 import { ReactComponent as SparkleIcon } from "@assets/icons/sparkle.svg";
 
 import { type IconName } from "@libs/types/types";
@@ -6,6 +11,11 @@ const iconNameToSvg: Record<
   IconName,
   React.FC<React.SVGProps<SVGSVGElement>>
 > = {
+  arch: ArchIcon,
+  diamond: DiamondIcon,
+  flower: FlowerIcon,
+  hexagon: HexagonIcon,
+  triangle: TriangleIcon,
   sparkle: SparkleIcon,
 };
 
