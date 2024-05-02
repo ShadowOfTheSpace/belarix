7443f530839215e60376f15472402e4a55e8581e
Shadow <oleksandr.vpro@gmail.com>
2024-05-02 08:42:25 +0300

    Add button pending state

--- src/libs/components/button/button.tsx
+++ src/libs/components/button/button.tsx
@@ -1,5 +1,6 @@
 import { ReactComponent as RightArrow } from "@assets/icons/right-arrow.svg";
 import { cn } from "@libs/utils/utils";
+import { motion } from "framer-motion";
 
 type Properties = {
   children?: React.ReactNode;
@@ -7,6 +8,7 @@ type Properties = {
   variant?: "default" | "arrow";
   className?: string;
   type?: "button" | "submit";
+  isPending?: boolean;
 };
 
 const Button: React.FC<Properties> = ({
@@ -15,19 +17,27 @@ const Button: React.FC<Properties> = ({
   children,
   className,
   type = "button",
+  isPending,
 }) => {
   return (
     <>
       {variant === "default" ? (
         <button
           className={cn(
-            "bg-white has-hover:hover:bg-transparent no-hover:active:bg-transparent px-[30px] py-[13px] rounded-[10px] w-[160px] md:w-auto font-medium text-[14px] text-bxm-black md:text-[16px] has-hover:hover:text-bxm-white no-hover:active:text-bxm-white leading-[normal] tracking-lighter transition-colors duration-300",
+            "relative bg-white has-hover:hover:bg-transparent no-hover:active:bg-transparent px-[30px] py-[13px] rounded-[10px] w-[160px] md:w-auto font-medium text-[14px] text-bxm-black md:text-[16px] has-hover:hover:text-bxm-white no-hover:active:text-bxm-white leading-[normal] tracking-lighter transition-colors duration-300 disabled:hover:bg-bxm-white",
             className
           )}
           type={type}
+          disabled={isPending}
           onClick={onClick}
         >
-          {children}
+          <motion.div animate={{ opacity: isPending ? 0 : 1 }}>
+            {children}
+          </motion.div>
+          <motion.div
+            animate={{ opacity: isPending ? 1 : 0 }}
+            className="absolute inset-0 border-[3px] m-auto border-t-transparent border-bxm-red rounded-full animate-spin size-[20px]"
+          ></motion.div>
         </button>
       ) : (
         <button
